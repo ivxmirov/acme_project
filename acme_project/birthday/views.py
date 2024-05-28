55097d04e7868d3d08d3c8e88318b40cc0b7ba7e
ivxmirov <ivxmirov@gmail.com>
2024-05-29 01:39:58 +0400

    after applying registration pages and access control

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import (
@@ -69,12 +71,25 @@ from .utils import calculate_birthday_countdown
 #     return render(request, 'birthday/birthday.html', context)
 
 
-class BirthdayCreateView(CreateView):
+class OnlyAuthorMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        object = self.get_object()
+        return object.author == self.request.user
+
+
+class BirthdayCreateView(LoginRequiredMixin, CreateView):
     model = Birthday
     form_class = BirthdayForm
 
+    def form_valid(self, form):
+        # Присвоить полю author объект пользователя из запроса.
+        form.instance.author = self.request.user
+        # Продолжить валидацию, описанную в форме.
+        return super().form_valid(form)
 
-class BirthdayDeleteView(DeleteView):
+
+class BirthdayDeleteView(OnlyAuthorMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
@@ -104,6 +119,15 @@ class BirthdayListView(ListView):
     paginate_by = 10
 
 
-class BirthdayUpdateView(UpdateView):
+class BirthdayUpdateView(OnlyAuthorMixin, UpdateView):
     model = Birthday
     form_class = BirthdayForm
+
+    # Определяем метод test_func() для миксина UserPassesTestMixin:
+    def test_func(self):
+        # Получаем текущий объект.
+        object = self.get_object()
+        # Метод вернёт True или False.
+        # Если пользователь - автор объекта, то тест будет пройден.
+        # Если нет, то будет вызвана ошибка 403.
+        return object.author == self.request.user
